53585b51267090a4cfdd5cc4067601b043397a1a
Jazz Whitfield <jazzwhitfield123@gmail.com>
2022-05-13 21:07:20 -0400

    Index and loop through each button and gridBox node. Create a conditional statement that renders the correct gridBox and displays the array based on the selection index.

--- script.js
+++ script.js
@@ -4,8 +4,47 @@ let gameBoardModule = (() => {
   return { gameBoard };
 })();
 
-//Setting up the displayController module
+//Setting up the displayController module to control who's turn it is
 let displayControllerModule = (() => {
+  const makeMove = document.querySelectorAll(".game-board-button");
+
+  //Start indexing and looping through each button node
+  let index = 0;
+  makeMove.forEach((makeMoves) => {
+    makeMoves.dataset.linkedButton = index;
+    makeMoves.addEventListener("click", renderArrayToScreen);
+
+    function renderArrayToScreen() {
+      const gridBoxes = document.querySelectorAll(".grid-box");
+
+      //Start indexing and looping through each grid box node
+      let index = 0;
+      gridBoxes.forEach((gridBox) => {
+        gridBox.dataset.linkedButton = index;
+
+        //Render clicked play on the correct grid box and display
+        if (
+          gridBox.getAttribute("data-linked-button") ==
+          makeMoves.getAttribute("data-linked-button")
+        ) {
+          gridBox.textContent =
+            gameBoardModule.gameBoard[gameBoardModule.gameBoard.length - 1];
+          console.log(
+            "show me my makeMoves linked button value...",
+            makeMoves.dataset.linkedButton
+          );
+          console.log(
+            "show me my gridBox linked button value...",
+            gridBox.dataset.linkedButton
+          );
+        }
+        index++;
+      });
+    }
+    index++;
+  });
+
+  // Test private function
   let testF = () => {
     console.log("testing private function call inside of a module object...");
   };
@@ -22,21 +61,6 @@ let createPlayer = (playerName, playerNumber, assignedXO) => {
   };
   return { getPlayerName, playerName, playerNumber, assignedXO };
 };
-
-//Setting up the render module for array to display on gameboard
-let renderArrayToScreenModule = (() => {
-  const gridBoxes = document.querySelectorAll(".grid-box");
-  gridBoxes[4].textContent = gameBoardModule.gameBoard;
-  console.log(
-    "show me the array as seen inside of renderArraryToScreenModeule...",
-    gameBoardModule.gameBoard
-  );
-  console.log(
-    "show me the node list of the gridBoxes as seen inside of renderArrayToScreenModule",
-    gridBoxes
-  );
-  return {};
-})();
 
 let Jay = createPlayer("Jay", 1, "X");
 let Jazz = createPlayer("Jazz", 1, "O");
